b3d30892ee30475a66c3fa7f99be0acede55c6bd
tien-li <sjia13579@gmail.com>
2024-04-09 18:08:54 -0400

    change board to 6 players

--- human_vs_human.py
+++ human_vs_human.py
@@ -6,5 +6,6 @@ want to play human-vs-human game
 """
 
 if __name__ == '__main__':
-    human_game = Game(p1_type='human', p2_type='human')
+    # human_game = Game(p1_type='human', p2_type='human')
+    human_game = Game(player_types=['human'] * 6)
     human_game.start()
